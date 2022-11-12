b4e034e6cc8f28609de6e203279ec4548a5b19ae
user42 <vintran@student.42.fr>
2022-11-12 19:21:39 +0100

    IRC & COMMANDS [...]

--- irc/commands/DIE.cpp
+++ irc/commands/DIE.cpp
@@ -7,7 +7,6 @@ void	Irc::_DIE(User & user, std::vector<std::string> & sCmd, std::vector<t_reply
 		serverReply.push_back(std::make_pair(user._fd, ERR_NOPRIVILEGES(user._nickName)));
 	else
 	{
-		// std::cout << LOGPREFIX << BIGREEN << "Server killed by " << user._nickName << WHITE << std::endl;
 		_serverKilled = true;
 		_killer = user._nickName;
 	}
